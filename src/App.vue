6887c14ac6d1b5b2bba2a2916509601c97644157
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 13:53:14 +0000

    Added new components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import FooterComponent from './components/FooterComponent.vue';
 import DescriptionComponent from './components/DescriptionComponent.vue';
+import InstructionComponent from './components/InstructionComponent.vue';
+import MainGame from './components/MainGame.vue';
+
 
 </script>
 
@@ -8,6 +11,8 @@ import DescriptionComponent from './components/DescriptionComponent.vue';
   <div>
     <h1>East Bunny Hunt</h1>
     <DescriptionComponent />
+    <MainGame />
+    <InstructionComponent />
     <FooterComponent />
   </div>
 </template>
