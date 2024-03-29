d7ea5675d796ea495dd5a4c95caa2dfb5deae66b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 14:49:48 +0000

    Updated logic in MainGame.vue

--- src/components/MainGame.vue
+++ src/components/MainGame.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="gameContainer">
-        Hello
+        <button 
+        v-if="playing"
+        @click="startGame">Go</button>
+        <button 
+        v-if="!playing"
+        @click="pauseGame">Pause</button>
+        <button 
+        v-if="playing"
+        @click="resetGame">Play Again</button>
         <AssetsComponent /> 
         <canvas ref="gameCanvas"></canvas>
     </div>
@@ -8,4 +16,36 @@
 
 <script setup lang="ts">
 import AssetsComponent from './AssetsComponent.vue';
+import { ref } from 'vue';
+
+//game logic
+const animationFrameId: { value?: number } = {};
+const playing = ref(true);
+const reset = ref(false);
+
+const animate = () => {
+    const loop = () => {
+    }
+    animationFrameId.value = requestAnimationFrame(loop);
+}
+
+const startGame = () => {
+    playing.value = !playing.value;
+    reset.value = false;
+    animate(); 
+}
+
+const pauseGame = () => {
+    playing.value = !playing.value;
+    if (animationFrameId.value !== undefined) {
+        cancelAnimationFrame(animationFrameId.value);
+    }
+}
+
+const resetGame = () => {
+    playing.value = true;
+    reset.value = true;
+    startGame();
+}
+
 </script>
