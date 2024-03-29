1f3ab20f29a212f8736cebac3470104fd1b8eba0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 13:53:03 +0000

    Created MainGame.vue

---
+++ src/components/MainGame.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="gameContainer">
+        Hello
+        <AssetsComponent /> 
+        <canvas ref="gameCanvas"></canvas>
+    </div>
+</template>
+
+<script lang="ts">
+import AssetsComponent from './AssetsComponent.vue';
+</script>
