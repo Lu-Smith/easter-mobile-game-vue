aac80f34f5c816f9129c418b4c6cddf513cce232
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 13:05:32 +0000

    Added DescriptionComponent.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import FooterComponent from './components/FooterComponent.vue';
+import DescriptionComponent from './components/DescriptionComponent.vue';
 
 </script>
 
 <template>
   <div>
     <h1>East Bunny Hunt</h1>
+    <DescriptionComponent />
     <FooterComponent />
   </div>
 </template>
