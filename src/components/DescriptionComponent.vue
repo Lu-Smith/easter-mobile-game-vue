269ac5d6ada979db3907da0b2e863ae7da334992
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 13:52:33 +0000

    Created DescritionComponent.vue

--- src/components/DescriptionComponent.vue
+++ src/components/DescriptionComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="gameDescription">
         <h2><span>Easter</span> Egg Hunt</h2>
-        <p>Easte Egg Hunt is a fun twist on the classic Space Invaders game, putting players in the hero role of the hunter Alvin.</p>
-        <p>Alvin is a Easter bunny who have to destroy all Easter Eggs and hunt down evil fluffy chicks to save the Easter realm.</p>
+        <p>Easter Egg Hunt is a fun twist on the classic Space Invaders game, putting players in the hero role of the hunter Alvin.</p>
+        <p>Alvin is a Easter bunny who have to destroy all Easter Eggs to hunt down evil fluffy chicks to save the Easter realm.</p>
         <p>Have an amazing adventure!</p>
     </div>
 </template>
