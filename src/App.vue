97b460c8ed60049962e4235ebdf3d49b59962f43
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 12:18:27 +0000

    Added FooterComponent to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import FooterComponent from './components/FooterComponent.vue';
+
 </script>
 
 <template>
   <div>
     <h1>East Bunny Hunt</h1>
+    <FooterComponent />
   </div>
 </template>
 
