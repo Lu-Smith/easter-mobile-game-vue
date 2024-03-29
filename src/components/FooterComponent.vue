b3d18171c3c306958ffe4c37dc17a97dd0410c01
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-29 12:18:11 +0000

    Created FooterComponent.vue

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Coded by 
+       <a
+           href='https://www.lunasmithart.com/' 
+           target='_blank'
+           className='portfolio'>
+           Luna Smith
+       </a>
+       -
+       <a 
+       href='https://github.com/Lu-Smith/easter-mobile-game-vue'
+       target='_blank'>
+           GitHub
+       </a>
+       .
+   </h3>
+</template>
