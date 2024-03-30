53b444f8d6538af9a11843cc0805561f8597bb47
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-30 19:02:03 +0000

    Created player.ts

---
+++ src/assets/classes/player.ts
@@ -0,0 +1,32 @@
+import Game from './game';
+
+export default class Player {
+    game: Game;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    spriteWidth: number;
+    spriteHeight: number;
+
+    constructor(game: Game) {
+        this.game =  game;
+        this.x = 100;
+        this.y = 0;
+        this.spriteWidth = 35;
+        this.spriteHeight = 35;
+        this.width = 0;
+        this.height = 0;
+    }
+    draw() {
+        this.game.context.beginPath();
+        this.game.context.arc(this.game.width * 0.5, this.game.height * 0.8, 
+            this.height, 0, Math.PI * 2);
+        this.game.context.stroke();
+    }
+    resize() {
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+    }
+}
