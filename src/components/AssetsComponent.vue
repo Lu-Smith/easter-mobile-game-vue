57bfe72376ffc933079ab02ac3f04b0e3251303a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-03 18:24:51 +0100

    Updated AssetsComponents.vue

--- src/components/AssetsComponent.vue
+++ src/components/AssetsComponent.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="assets">
         <img src="../assets//media/images/Background_Easter.png" alt="background" id="background1" />
+        <img src="../assets//media/images/eggs.png" alt="eggs" id="eggs" />
+        <img src="../assets//media/images/player_Bunny.png" alt="bunny" id="player" />
     </div>
 </template>
