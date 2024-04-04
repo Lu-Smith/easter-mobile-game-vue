f00ff7510930f64f2f73940c4edee4ea5668e8ba
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-04 12:48:11 +0100

    Updated eggs.ts

--- src/assets/classes/eggs.ts
+++ src/assets/classes/eggs.ts
@@ -1,30 +1,30 @@
-// import Enemy from './enemy';
-// import Game from './game';
+import Enemy from './enemy';
+import Game from './game';
 
-// export default class Eggs extends Enemy {
-//     image: HTMLImageElement;
-//     frameX: number;
-//     frameY1: number;
-//     frameY2: number;
-//     frameY3: number;
-//     frameY4: number;
-//     lives: number;
-//     maxFrame: number;
-//     maxLives: number;
+export default class Eggs extends Enemy {
+    image: HTMLImageElement;
+    frameX: number;
+    frameY1: number;
+    frameY2: number;
+    frameY3: number;
+    frameY4: number;
+    lives: number;
+    maxFrame: number;
+    maxLives: number;
 
-//     constructor(game: Game, positionX: number, positionY: number) {
-//         super(game, positionX, positionY);
-//         this.image = document.getElementById('eggs') as HTMLImageElement; 
-//         this.frameX = 0;
-//         this.maxFrame = 2;
-//         this.frameY1 = 0;
-//         this.frameY2 = 1;
-//         this.frameY3 = 2;
-//         this.frameY4 = 3;
-//         this.lives = 1;
-//         this.maxLives = this.lives;
-//     }
-//     resize() {
-//         this.image = document.getElementById('eggs') as HTMLImageElement;
-//     }
+    constructor(game: Game, positionX: number, positionY: number) {
+        super(game, positionX, positionY);
+        this.image = document.getElementById('eggs') as HTMLImageElement; 
+        this.frameX = 0;
+        this.maxFrame = 2;
+        this.frameY1 = 0;
+        this.frameY2 = 1;
+        this.frameY3 = 2;
+        this.frameY4 = 3;
+        this.lives = 1;
+        this.maxLives = this.lives;
+    }
+    resize() {
+        this.image = document.getElementById('eggs') as HTMLImageElement;
+    }
 }
