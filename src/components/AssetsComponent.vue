e716bd3107c87e89a0e142df7ee55c71fa69d671
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-07 20:01:03 +0100

    Removed unnecessary background

--- src/components/AssetsComponent.vue
+++ src/components/AssetsComponent.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="assets">
-        <img src="../assets//media/images/Background_Easter.png" alt="background" id="background1" />
         <img src="../assets//media/images/eggs.png" alt="eggs" id="eggs" />
         <img src="../assets//media/images/chicks.png" alt="chicks" id="chicks" />
         <img src="../assets//media/images/player_Bunny.png" alt="bunny" id="player" />
