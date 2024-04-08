1b1985bb7d851f9fa7e7b35ed465b190577c0d89
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-08 17:16:15 +0100

    Updated background.ts

--- src/assets/classes/background.ts
+++ src/assets/classes/background.ts
@@ -63,34 +63,38 @@ export default class Background {
         
         // Draw the second star
         context.beginPath();
-        context.moveTo(this.x * 4 + 20, this.y * 1.3 + 33);
-        context.lineTo(this.x * 4 + 24, this.y * 1.3 + 40);
-        context.lineTo(this.x * 4 + 34, this.y * 1.3 + 42);
-        context.lineTo(this.x * 4 + 28, this.y * 1.3 + 50);
-        context.lineTo(this.x * 4 + 30, this.y * 1.3 + 60);
-        context.lineTo(this.x * 4 + 22, this.y * 1.3 + 55);
-        context.lineTo(this.x * 4 + 10, this.y * 1.3 + 60);
-        context.lineTo(this.x * 4 + 12, this.y * 1.3 + 50);
-        context.lineTo(this.x * 4 + 6, this.y * 1.3 + 42);
-        context.lineTo(this.x * 4 + 16, this.y * 1.3 + 40);
+        context.moveTo(this.x * 4 + (20/3), this.y * 1.3 + (33/3));
+        context.lineTo(this.x * 4 + (24/3), this.y * 1.3 + (40/3));
+        context.lineTo(this.x * 4 + (34/3), this.y * 1.3 + (42/3));
+        context.lineTo(this.x * 4 + (28/3), this.y * 1.3 + (50/3));
+        context.lineTo(this.x * 4 + (30/3), this.y * 1.3 + (60/3));
+        context.lineTo(this.x * 4 + (22/3), this.y * 1.3 + (55/3));
+        context.lineTo(this.x * 4 + (10/3), this.y * 1.3 + (60/3));
+        context.lineTo(this.x * 4 + (12/3), this.y * 1.3 + (50/3));
+        context.lineTo(this.x * 4 + (6/3), this.y * 1.3 + (42/3));
+        context.lineTo(this.x * 4 + (16/3), this.y * 1.3 + (40/3));
         context.closePath();
         context.fill();
         context.restore();
 
         // Draw the third star
+        context.save(); 
+        context.translate(this.x * 7 + 2, this.y * 1.8 + 3); 
+        context.rotate((30 * Math.PI) / 160); 
         context.beginPath();
-        context.moveTo(this.x * 7 + 10, this.y * 1.8 + 15);
-        context.lineTo(this.x * 7 + 12, this.y * 1.8 + 20);
-        context.lineTo(this.x * 7 + 17, this.y * 1.8 + 21);
-        context.lineTo(this.x * 7 + 14, this.y * 1.8 + 25);
-        context.lineTo(this.x * 7 + 15, this.y * 1.8 + 32);
-        context.lineTo(this.x * 7 + 10, this.y * 1.8 + 27.5);
-        context.lineTo(this.x * 7 + 5, this.y * 1.8 + 30);
-        context.lineTo(this.x * 7 + 6, this.y * 1.8 + 25);
-        context.lineTo(this.x * 7 + 3, this.y * 1.8 + 21);
-        context.lineTo(this.x * 7 + 8, this.y * 1.8 + 20);
+        context.moveTo(-5, -12); 
+        context.lineTo(-3, -7);
+        context.lineTo(2, -6);
+        context.lineTo(-1, -2);
+        context.lineTo(0, 5);
+        context.lineTo(-5, 0);
+        context.lineTo(-10, 5);
+        context.lineTo(-9, -2);
+        context.lineTo(-12, -6);
+        context.lineTo(-7, -7);
         context.closePath();
         context.fill();
+        context.restore(); 
 
         // Draw the four star
         context.beginPath();
@@ -109,16 +113,16 @@ export default class Background {
 
         // Draw the fifth star
         context.beginPath();
-        context.moveTo(this.x * 18 + 20, this.y * 2.4 + 33);
-        context.lineTo(this.x * 18 + 24, this.y * 2.4 + 40);
-        context.lineTo(this.x * 18 + 34, this.y * 2.4 + 42);
-        context.lineTo(this.x * 18 + 28, this.y * 2.4 + 50);
-        context.lineTo(this.x * 18 + 30, this.y * 2.4 + 60);
-        context.lineTo(this.x * 18 + 22, this.y * 2.4 + 55);
-        context.lineTo(this.x * 18 + 10, this.y * 2.4 + 60);
-        context.lineTo(this.x * 18 + 12, this.y * 2.4 + 50);
-        context.lineTo(this.x * 18 + 6, this.y * 2.4 + 42);
-        context.lineTo(this.x * 18 + 16, this.y * 2.4 + 40);
+        context.moveTo(this.x * 18 + (20/2.5), this.y * 2.4 + (33/2.5));
+        context.lineTo(this.x * 18 + (24/2.5), this.y * 2.4 + (40/2.5));
+        context.lineTo(this.x * 18 + (34/2.5), this.y * 2.4 + (42/2.5));
+        context.lineTo(this.x * 18 + (28/2.5), this.y * 2.4 + (50/2.5));
+        context.lineTo(this.x * 18 + (30/2.5), this.y * 2.4 + (60/2.5));
+        context.lineTo(this.x * 18 + (22/2.5), this.y * 2.4 + (55/2.5));
+        context.lineTo(this.x * 18 + (10/2.5), this.y * 2.4 + (60/2.5));
+        context.lineTo(this.x * 18 + (12/2.5), this.y * 2.4 + (50/2.5));
+        context.lineTo(this.x * 18 + (6/2.5), this.y * 2.4 + (42/2.5));
+        context.lineTo(this.x * 18 + (16/2.5), this.y * 2.4 + (40/2.5));
         context.closePath();
         context.fill();
         context.restore();
