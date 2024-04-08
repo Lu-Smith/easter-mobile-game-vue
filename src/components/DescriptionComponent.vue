1c9c23ddbde942fa2c96aa91135692e279de4d59
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-08 15:39:48 +0100

    Updated styling in DescriptionComponent.vue

--- src/components/DescriptionComponent.vue
+++ src/components/DescriptionComponent.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="gameDescription">
-        <h2><span>Easter</span> Egg Hunt</h2>
         <p>Easter Egg Hunt is a fun twist on the classic Space Invaders game, putting players in the hero role of the hunter Alvin.</p>
         <p>Alvin is a Easter bunny who have to destroy all Easter Eggs to hunt down evil fluffy chicks to save the Easter realm.</p>
         <p>Have an amazing adventure!</p>
     </div>
 </template>
+
+<style scoped>
+.gameDescription{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 10px;
+    margin: 0 50px;
+}
+</style>
