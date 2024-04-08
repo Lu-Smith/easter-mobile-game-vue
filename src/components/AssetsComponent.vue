fe3aa4a9cd939b35f324fc92b35f1cbe8f156927
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-08 18:33:11 +0100

    Added new sounds to AssetsComponents.vue

--- src/components/AssetsComponent.vue
+++ src/components/AssetsComponent.vue
@@ -3,5 +3,12 @@
         <img src="../assets//media/images/eggs.png" alt="eggs" id="eggs" />
         <img src="../assets//media/images/chicks.png" alt="chicks" id="chicks" />
         <img src="../assets//media/images/player_Bunny.png" alt="bunny" id="player" />
+        <audio :src=Shoot id="shoot"></audio>
+        <audio :src=Lost id="lose"></audio>
     </div>
 </template>
+
+<script setup lang="ts">
+    import Shoot from '../assets/media/sounds/shoot.wav';
+    import Lost from '../assets/media/sounds/lose.wav';
+</script>
