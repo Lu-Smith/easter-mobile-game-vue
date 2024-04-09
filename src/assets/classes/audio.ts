ab69a203bb0df7f77ac8ebeb24c6e009a53182d8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-09 14:40:47 +0100

    Added two new sounds

--- src/assets/classes/audio.ts
+++ src/assets/classes/audio.ts
@@ -1,10 +1,14 @@
 export default class Audio {
     shoot: HTMLAudioElement;
     lose: HTMLAudioElement;
+    crack: HTMLAudioElement;
+    die: HTMLAudioElement;
 
     constructor() {
         this.shoot = document.getElementById('shoot') as HTMLAudioElement;
         this.lose = document.getElementById('lose') as HTMLAudioElement;
+        this.crack = document.getElementById('crack') as HTMLAudioElement;
+        this.die = document.getElementById('die') as HTMLAudioElement;
     }
     play(sound: HTMLAudioElement ) {
         sound.currentTime = 0;
