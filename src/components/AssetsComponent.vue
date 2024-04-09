6dc11ef33046aa153203524a735c238be66fd429
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-09 14:41:16 +0100

    Added two new sounds to AssetsComponents.vue

--- src/components/AssetsComponent.vue
+++ src/components/AssetsComponent.vue
@@ -5,10 +5,14 @@
         <img src="../assets//media/images/player_Bunny.png" alt="bunny" id="player" />
         <audio :src=Shoot id="shoot"></audio>
         <audio :src=Lost id="lose"></audio>
+        <audio :src=Crack id="crack"></audio>
+        <audio :src=Die id="die"></audio>
     </div>
 </template>
 
 <script setup lang="ts">
     import Shoot from '../assets/media/sounds/shoot.wav';
+    import Crack from '../assets/media/sounds/crack.wav';
+    import Die from '../assets/media/sounds/die.wav';
     import Lost from '../assets/media/sounds/lose.wav';
 </script>
